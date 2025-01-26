00ed85154c91f94cc2277bf366c4b0879e986250
satyam-trimale <trimalesatyam@gmail.com>
2025-01-27 05:23:04 +0530

    Util codes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 // require('dotenv').config({path:'./env'})
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 // import mongoose, { connect } from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
 
-
-
+})
+.catch((err) => {
+    console.log("MG=ongoDB connection failed",err);
+})
 
 // import express from "express";
 // const app = express()(async () => {
