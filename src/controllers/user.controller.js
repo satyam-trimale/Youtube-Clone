54c63c1afde7d074bd8d549fc66ce009ee52f954
satyam-trimale <trimalesatyam@gmail.com>
2025-01-31 17:03:31 +0530

    fix all register cintroller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -37,7 +37,7 @@ const registerUser = asyncHandler(async(req,res)=>{
 
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath = req.files.coverImage[0].path;
+        coverImageLocalPath = req.files.coverImage[0].path
     }
 
     if(!avatarLocalPath){
