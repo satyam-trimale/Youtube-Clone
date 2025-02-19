373f3aca78f1b368ec297f66eab38060bbc7a9b4
satyam-trimale <trimalesatyam@gmail.com>
2025-02-20 01:46:47 +0530

    Add healthcheck route and controller; implement basic healthcheck response

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ app.use(cookieParser())
 
 //routes
 import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 import videoRouter from './routes/video.routes.js'
 import tweetRouter from './routes/tweet.routes.js'
 import commentRouter from './routes/comment.routes.js'
@@ -22,7 +23,9 @@ import subscriptionRouter from './routes/subscription.routes.js'
 import likeRouter from './routes/like.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 
+
 //routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/videos",videoRouter)
 app.use("/api/v1/tweets",tweetRouter)
